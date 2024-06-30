c444b5ffb1c879d2ae5a9010d19a93062edc3894
Sean Murphy <smurphy748@gmail.com>
2024-06-30 19:41:33 -0400

    remove packages so scripts can be run independantly

--- sean_resources.py
+++
@@ -1,8 +0,0 @@
-from scipy.stats import pearsonr
-import matplotlib.pyplot as plt
-
-def corrfunc(x, y, ax=None, **kws):
-    """Plot the correlation coefficient in the top left hand corner of a plot."""
-    r, _ = pearsonr(x, y)
-    ax = ax or plt.gca()
-    ax.annotate(f'ρ = {r:.2f}', xy=(.1, .9), xycoords=ax.transAxes, color = "red", fontsize = 15)
